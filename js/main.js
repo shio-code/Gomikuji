0f48aa33ad4931dcf1c0275b83f82496724fee94
shio-code <74956257+shio-code@users.noreply.github.com>
2021-05-03 11:17:44 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+{
+  const btn = document.getElementById('btn');
+  
+  btn.addEventListener('click',()=>{
+    const n =Math.random();
+    if(n <0.04){
+      btn.textContent ='大吉';
+    }else if(n<0.2){
+      btn.textContent ='凶';
+    }else{
+      btn.textContent ='中吉';
+    }
+    
+  });
+}
